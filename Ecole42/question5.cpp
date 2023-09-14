cf01ffeb11658b3bf8ed6c0bc7d1b48e45f79668
ErgeSooN <65658818+ErgeSooN@users.noreply.github.com>
2023-09-14 16:27:08 +0300

    Ecole 42

--- Ecole42/question5.cpp
+++ Ecole42/question5.cpp
@@ -5,151 +5,127 @@
 #include <vector>
 #include <set>
 #include <locale>
+#include <algorithm>
 using namespace std;
 
 
 
 /**
- * @param instructions The list of instructions as memorized by the mutant.
- * @param target The coordinates (x, y) of the target.
- * @return A string respecting the given format to fix the mutant's path.
+ * @param n_gears An integer representing the number of gears in the system (numbered from 0 to N-1).
+ * @param connections An array representing all pairs of gears connected together.
+ * @return An array of two integers representing the number of gears rotating clockwise and counterclockwise respectively, or [-1, -1] if the configuration is invalid.
  */
-string findCorrectPath(vector<string> instructions, vector<int> target) {
-  // Write your code here
-  // Şu şekilde çalışıyor:
-    //return "Replace instruction 3 with FORWARD"; // 3.indexi FORWARD ile değiştir.
+vector<int> gearBalance(int gears, vector<vector<int>> VEC) {
+  int left = 0;
+  int right = 0;
+  map<int, int>::iterator it;
+  map<int, int>::iterator ite;
+  map<int, int> end; // <Sayı, left/right>
+  map<int, vector<int>>map; // <Sayı, <Next, Next, Next>>
+  size_t vectorSize = VEC.size();
+  size_t size;
 
-  //Mutant belirli bir pathi biliyor lakin yanlış biliyor,
-  //  Bizim mutant'ın bildiği pathi değiştirip, hedefe götürmemiz gerekiyor.
-  vector<int> loc; // Mutantın konumu
-  loc.push_back(0); // x
-  loc.push_back(0); // y
-  std::string direc = "E"; // Mutant sağa bakmaktadır
-  // Kuzey, Güney, Doğu, Batı değerlerini alabilir.
-  // N, S, E, W
-
-  // ilk cevap: "Replace instruction 1 with TURN RIGHT"
-
-  vector<std::string> ac;
-  ac.push_back("FORWARD");
-  ac.push_back("BACK");
-  ac.push_back("TURN LEFT");
-  ac.push_back("TURN RIGHT");
-
-  std::string index;
-  std::string action;
-  bool run = true;
-  vector<std::string> old = instructions;
-  size_t size = old.size();
-
-  for (size_t k = 0; k < size && run != false; ++k) // adımları tek tek değiştirip deneyecek
+  for (size_t i = 0; i < vectorSize; ++i)
   {
-    for (size_t l = 0; l < 4 && run != false; ++l) // 4 farklı durum var, herbirini koyup tek tek deneyecek
+    map[VEC[i][0]].push_back(VEC[i][1]);
+    map[VEC[i][1]].push_back(VEC[i][0]);
+  }
+
+  end[0] = 0;
+  int value = 0;
+  for (auto it = map.begin(); it != map.end(); ++it)
+  {
+    ite = end.find(it->first);
+    if (ite != end.end())
     {
-      instructions[k] = ac[l];
-      loc[0] = 0;
-      loc[1] = 0;
-      direc = "E";
-      for (size_t i = 0; i < size && run != false; ++i)
-      {
-        if (instructions[i] == "FORWARD"){
-          if (direc == "N")
-            loc[1]++;
-          else if (direc == "S")
-            loc[1]--;
-          else if (direc == "E")
-            loc[0]++;
-          else
-            loc[0]--;
-        }
-        else if (instructions[i] == "BACK"){
-          if (direc == "N")
-            loc[1]--;
-          else if (direc == "S")
-            loc[1]++;
-          else if (direc == "E")
-            loc[0]--;
-          else
-            loc[0]++;
-        }
-        else if (instructions[i] == "TURN LEFT"){
-          if (direc == "N")
-            direc = "W";
-          else if (direc == "S")
-            direc = "E";
-          else if (direc == "E")
-            direc = "N";
-          else
-            direc = "S";
-        }
-        else if (instructions[i] == "TURN RIGHT"){
-          if (direc == "N")
-            direc = "E";
-          else if (direc == "S")
-            direc = "W";
-          else if (direc == "E")
-            direc = "S";
-          else
-            direc = "N";
-        }
-
-        if (i + 1 == size && target[0] == loc[0] && target[1] == loc[1])
-        {
-          action = ac[l];
-          index = std::to_string(k + 1);
-          return ("Replace instruction " + index + " with " + action);
-          run = false;
-        }
-      }
-      instructions[k] = old[k];
-      loc[0] = 0;
-      loc[1] = 0;
-      direc = "E";
-
+      if (end[ite->first] == 1)
+        value = 0;
+      else
+        value = 1;
+      size = it->second.size();
+      for (int i = 0; i < size; ++i)
+        end[it->second[i]] = value;
+    }
+    if (std::next(it) == map.end() && end.size() != gears)
+    {
+      it = map.begin();
     }
   }
 
-  // std::cout << loc[0] << "-" << loc[1] << std::endl;
 
-  // std::string index = "1"; // döngü kontrolü için indexi 8 yap.
-  // std::string action = "TURN RIGHT";
-  return ("Replace instruction " + index + " with " + action);
-}
+  for (size_t i = 0; i < vectorSize; ++i)
+  {
+    it = end.find(VEC[i][0]);
+    ite = end.find(VEC[i][1]);
+    if (it->second == ite->second)
+      return {-1, -1};
+   }
 
-/* Ignore and do not change the code below */
-
-void trySolution(string recipe) {
-  Json::Value output_json;
-  output_json = Json::Value(recipe);
-
-  Json::FastWriter fastWriter;
-  std::string output_str = fastWriter.write(output_json);
-  cout << "" << output_str.c_str();
-}
-
-int main () {
-  setlocale(LC_ALL, "en_US.UTF-8");
-  Json::Reader reader;
-  string line;
-  Json::Value parsed_json;
-  std::getline(std::cin, line, '\n');
-  reader.parse(line, parsed_json);
-
-  vector<string> array;
-  for (int i = 0 ; i < parsed_json.size() ; i++) {
-    array.push_back(parsed_json[i].asString());
+  // for (auto it = map.begin(); it != map.end(); ++it)
+  // {
+  //   std::cout << it->first << "-> ";
+  //   size = it->second.size();
+  //   for (size_t i = 0; i < size; ++i)
+  //   {
+  //     std::cout << it->second[i] << " * ";
+  //   }
+  //   std::cout << std::endl;
+  // }
+  // std::cout << "------------------" << std::endl;
+  for (auto it = end.begin(); it != end.end(); ++it)
+  {
+    // std::cout << "->:" << it->first << "-" << it->second << std::endl;
+    if (it->second == 0)
+      right++;
+    else if (it->second == 1)
+      left++;
   }
-  vector<string> instructions = array;
-  std::getline(std::cin, line, '\n');
-  reader.parse(line, parsed_json);
 
-  vector<int> array_2;
-  for (int i = 0 ; i < parsed_json.size() ; i++) {
-    array_2.push_back(parsed_json[i].asInt());
-  }
-  vector<int> target = array_2;
+  return {right, left};
 
-  string output = findCorrectPath(instructions, target);
-  trySolution(output);
+
 }
-/* Ignore and do not change the code above */
+
+  /* Ignore and do not change the code below */
+
+  void trySolution(vector<int> output) {
+    Json::Value output_json;
+
+    output_json = Json::Value(Json::arrayValue);
+    Json::Value current_value;
+    for (int i = 0 ; i < output.size() ; i++) {
+      current_value = Json::Value(output[i]);
+      output_json.append(current_value);
+    }
+
+    Json::FastWriter fastWriter;
+    std::string output_str = fastWriter.write(output_json);
+    cout << "" << output_str.c_str();
+  }
+
+  int main () {
+    setlocale(LC_ALL, "en_US.UTF-8");
+    Json::Reader reader;
+    string line;
+    Json::Value parsed_json;
+    std::getline(std::cin, line, '\n');
+    reader.parse(line, parsed_json);
+    int n_gears = parsed_json.asInt();
+    std::getline(std::cin, line, '\n');
+    reader.parse(line, parsed_json);
+
+    vector<vector<int>> array;
+    for (int i = 0 ; i < parsed_json.size() ; i++) {
+    
+      vector<int> array2;
+      for (int j = 0 ; j < parsed_json[i].size() ; j++) {
+        array2.push_back(parsed_json[i][j].asInt());
+      }
+      array.push_back(array2);
+    }
+    vector<vector<int>> connections = array;
+
+    vector<int> output = gearBalance(n_gears, connections);
+    trySolution(output);
+  }
+  /* Ignore and do not change the code above */
